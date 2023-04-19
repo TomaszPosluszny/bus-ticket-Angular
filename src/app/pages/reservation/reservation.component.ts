4a54b90c3d5dbcb9c2ee8aac7b8f162d47e17672
TomaszPosluszny <tomkposluszny@gmail.com>
2023-04-19 20:06:09 +0200

    Add style in temperatre and add variable name city
    Add temperatre in the top page and provide variable city from Reservation component to Weather service.

--- src/app/pages/reservation/reservation.component.ts
+++ src/app/pages/reservation/reservation.component.ts
@@ -4,22 +4,39 @@ import { WeatherService } from 'src/app/services/weather.service';
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
-  styleUrls: ['./reservation.component.scss']
+  styleUrls: ['./reservation.component.scss'],
 })
 export class ReservationComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
 
-  constructor( private weatherService:WeatherService
+  dataTemp: any = '';
+  kalwinTemp: number = 273.15;
+  miasto:string = 'Warszawa'
+ 
+  
+  
+  ngOnInit(): void {this.weatherService.city = 'Wrocław';
     
-  ) { }
-dataTemp:any ={}
-  ngOnInit(): void {this.weatherService.getCity().subscribe( (data:any) => {
-    this.dataTemp = data
-    console.log(this.dataTemp.main.temp)
-    })
-    }
+    this.weatherService.getCity().subscribe((data: any) => {
+      this.dataTemp = data;
+      
+      
+    });
+  }
 
   planeUrl: string =
-  'https://raw.githubusercontent.com/TomaszPosluszny/airline-ticket-reservation-Angular/f18518988e93d1928a12814495e700ed15493023/src/app/images/plane.jpg';
+    'https://raw.githubusercontent.com/TomaszPosluszny/airline-ticket-reservation-Angular/f18518988e93d1928a12814495e700ed15493023/src/app/images/plane.jpg';
   description: string = ' This is plane';
-  
+
+  nameCites() {
+     
+    return (
+      this.weatherService.city)
+
+  }
+  tempCites() {
+    return (
+     Math.round(this.dataTemp.main?.temp - this.kalwinTemp)
+    );
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,14 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReservationComponent } from '../pages/reservation/reservation.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
+ 
+  city:string = ''
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,  ) { }
   getCity() {
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=Wrocław&appid=f9fe3ecd6dcb482c2724f8261001d8e4');
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q='+ this.city +'&appid=f9fe3ecd6dcb482c2724f8261001d8e4');
   }
 }
