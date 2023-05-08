7e12cbfc35aecd8ac6c69a9b749444138c920a9d
TomaszPosluszny <tomkposluszny@gmail.com>
2023-05-08 20:34:40 +0200

    Add current temperature date and time
    Add this information in nav component. Add new style nav__temp and nav__data.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
 
-  constructor() { }
+  today:any = new Date();
+  dataTemp: any = '';
+  kalwinTemp: number = 273.15;
+  start: any = 'Wrocław';
 
+  nameCites() {
+    return this.start;
+  }
+  tempCites() {
+    return Math.round(this.dataTemp.main?.temp - this.kalwinTemp);
+  }
+  changedDate:any = '';
+  pipe = new DatePipe('en-US');
+  changeFormat(today:any){
+    let ChangedFormat = this.pipe.transform(this.today, 'dd/MM/YYYY');
+    this.changedDate = ChangedFormat;
+    console.log(this.changedDate);
+  }
   ngOnInit(): void {
+    this.weatherService.nameCity = 'Wrocław';
+    this.weatherService.getCity().subscribe((data: any) => {
+      this.dataTemp = data;
+      console.log(this.dataTemp);
+    });
   }
 
   logoUrl: string =
-  'https://raw.githubusercontent.com/TomaszPosluszny/airline-ticket-reservation-Angular/master/src/app/components/images/logo.png';
+    'https://raw.githubusercontent.com/TomaszPosluszny/airline-ticket-reservation-Angular/master/src/app/components/images/logo.png';
   description: string = ' This is plane';
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -14,6 +14,8 @@ const API_KEY = "f9fe3ecd6dcb482c2724f8261001d8e4";
 export class WeatherService {
   constructor(private http: HttpClient) {}
 
+  nameCity:string = ''
+
   getCities$: Observable<City[]> = this.http
     .get<City[]>("/assets/cities.json")
     .pipe(
@@ -32,6 +34,10 @@ export class WeatherService {
         //TODO to remove, for quick debug only
         tap(r => console.log(r))
       );
+
+      getCity() {
+        return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.nameCity}&appid=f9fe3ecd6dcb482c2724f8261001d8e4`);
+      }
 }
 
  
@@ -41,4 +47,3 @@ export class WeatherService {
 //   getCity() {
 //     return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.tomek}&appid=f9fe3ecd6dcb482c2724f8261001d8e4`);
 //   }
-
